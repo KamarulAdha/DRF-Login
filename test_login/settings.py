9c322dcb1eca0a360cdfebeeb92b1fce68d003cf
Kamarul Adha <kamarul.adha360@gmail.com>
2020-08-25 05:17:08 +0800

    staticfiles

--- test_login/settings.py
+++ test_login/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 from datetime import timedelta
 
@@ -140,7 +140,7 @@ SIMPLE_JWT = {
 
 }
 
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'profiles_api.UserProfile'
